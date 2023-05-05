e87b0b84b88685789b62de81c8645bbaccbeafb5
bruhng <bruhng@student.chalmers.se>
2023-05-05 11:49:14 +0200

    Fix wasi support

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,25 +3,25 @@ import { JSON } from "../node_modules/assemblyscript-json/assembly/index";
 
 import {Console, CommandLine} from "../node_modules/as-wasi/assembly/";
 
+const args = new CommandLine(); 
+
+main();
 
 
-
-
-export function main(): void {
+export function main(): i32 {
     
-    const args = new CommandLine();
+    const args = new CommandLine();    
 
-
-    const action = args.get(0);
-    //Console.log("action: " + action!.toString())
+    const action = args.get(1);
     if (action == "manifest") {
         manifest();
     } else if (action == "transform") {
-        transform(args.get(1)!, args.get(2)!);
+        transform(args.get(2)!, args.get(3)!);
     } else {
         Console.error("invalid action " + action!)
     }
 
+    return 0;
 }
 
 function transform(from: string, to: string): void {
@@ -38,21 +38,21 @@ function transform_vigenere(to: string): void {
         Console.error("stdin is null");
         return;
     }
-    let jsonObj: JSON.Obj = <JSON.Obj>(JSON.parse(stdin));
+    //let jsonObj: JSON.Obj = <JSON.Obj>(JSON.parse(stdin));
 
-    let text = jsonObj.getString("text");
-    let key = jsonObj.getString("key");
+    //let text = jsonObj.getString("text");
+    //let key = jsonObj.getString("key");
 
-    if (text == null || key == null) {
-        Console.error("text or key is null");
-    }
+    //if (text == null || key == null) {
+    //    Console.error("text or key is null");
+    //}
 
     if (to == "html") {
 
     }else if(to == "latex") {
 
 
-        let string = '[{"name": "raw", "data": ' + text!.toString() + '}]'
+        let string = '[{"name": "raw", "data": ' + "hello world" + '}]'
         let result = <JSON.Obj>(JSON.parse(string));
 
         Console.log(result.stringify());
@@ -65,10 +65,5 @@ function transform_vigenere(to: string): void {
 
 
 function manifest(): void {
-    //const output: JSON.Obj =
-        //<JSON.Obj>(
-            //JSON.parse('{"name":"vigenere","version":"0.1","description":"this package support vigenere modules which can be used to encrypt text","transforms":[{"from":"vigenere","to":["html","latex"],"arguments":[{"name":"key","default":"","description":"key used to decrypt text"}]}]}'
-            //));
-    //let json = output.stringify();
     Console.log('{"name":"vigenere","version":"0.1","description":"this package support vigenere modules which can be used to encrypt text","transforms":[{"from":"vigenere","to":["html","latex"],"arguments":[{"name":"key","default":"","description":"key used to decrypt text"}]}]}');
 }

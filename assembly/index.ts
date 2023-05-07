2ec8dc59fe12a9e34d9a44e49c6663c2e25eb148
bruhn <bruhng@student.chalmers.se>
2023-05-08 00:37:08 +0200

    Add skeleton code and encrypt function, working in theory

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,12 +3,11 @@ import { JSON } from "../node_modules/assemblyscript-json/assembly/index";
 
 import {Console, CommandLine} from "../node_modules/as-wasi/assembly/";
 
-const args = new CommandLine(); 
 
 main();
 
 
-export function main(): i32 {
+export function main(): void {
     
     const args = new CommandLine();    
 
@@ -21,14 +20,13 @@ export function main(): i32 {
         Console.error("invalid action " + action!)
     }
 
-    return 0;
 }
 
 function transform(from: string, to: string): void {
     if (from == "vigenere") {
         transform_vigenere(to);
     }else{
-        Console.error("package does not support" + from)
+        Console.error("package does not support " + from)
     }
 }
 
@@ -38,29 +36,57 @@ function transform_vigenere(to: string): void {
         Console.error("stdin is null");
         return;
     }
-    //let jsonObj: JSON.Obj = <JSON.Obj>(JSON.parse(stdin));
 
-    //let text = jsonObj.getString("text");
-    //let key = jsonObj.getString("key");
+    const data = <JSON.Obj>(JSON.parse(stdin));
+    const text: string = data.getString("data")!.toString();
+    const args = <JSON.Obj>(data.get("arguments"));
+    const key = args.getString("key")!.toString();
+    
+    const encrypted = encrypt_vigenere(text, key);
 
-    //if (text == null || key == null) {
-    //    Console.error("text or key is null");
-    //}
+    let string = "";
 
     if (to == "html") {
-
+        string = '[{"name": "raw", "data": "<p>' + encrypted + '</p>"}]'
     }else if(to == "latex") {
-
-
-        let string = '[{"name": "raw", "data": ' + "hello world" + '}]'
-        let result = <JSON.Obj>(JSON.parse(string));
-
-        Console.log(result.stringify());
+        string = '[{"name": "raw", "data": ' + encrypted + '}]'
     }
-
+    let result = <JSON.Obj>(JSON.parse(string));
+    Console.log(result.stringify());
 }
 
+function encrypt_vigenere(s: string, key: string): string {
+    let result = "";
+    let nonNumerical = 0;
+    for (let i = 0; i < s.length; i++) {
+        let c = s.charCodeAt(i);
+        let k = key.charCodeAt((i-nonNumerical)% key.length);
+        
+        if (isAlphabetic(s.charAt(i))) {
+            let stringOffset = isUpperCase(s.charAt(i)) ? 65:97;
+            let keyOffset = isUpperCase(key.charAt((i-nonNumerical) % key.length)) ? 65 : 97;
+            
 
+            let encryptedCharCode = (c - stringOffset + (k -keyOffset)) % 26 + stringOffset;
+
+            result += String.fromCharCode(encryptedCharCode);
+        } else {
+            result += s.charAt(i);
+            nonNumerical++;
+        }
+    }
+    return result;
+}
+
+function isUpperCase(char: string): bool {
+    let code = char.charCodeAt(0);
+    return code >= 65 && code <= 90;
+  }
+
+function isAlphabetic(char: string): bool {
+  let code = char.charCodeAt(0);
+  return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+}
 
 
 

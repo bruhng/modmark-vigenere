1de1a440e2f25997ffa071e0f0dd39ad82cdd53a
bruhn <bruhng@student.chalmers.se>
2023-05-08 17:25:52 +0200

    Add decrypt support

--- assembly/index.ts
+++ assembly/index.ts
@@ -47,12 +47,19 @@ function transform_vigenere(to: string): void {
     const text: string = data.getString("data")!.toString();
     const args = <JSON.Obj>(data.get("arguments"));
     const key = args.getString("key")!.toString();
-    
-    const encrypted = encrypt_vigenere(text, key);
+    const decrypt = args.getString("decrypt")!.toString() == "true";
+
+
+    let crypted = "";
+    if(decrypt){
+        crypted = decrypt_vigenere(text, key);
+    }else{
+        crypted = encrypt_vigenere(text, key);
+    }
 
     let string = "";
 
-    let split = encrypted.split("\n");
+    let split = crypted.split("\n");
 
     if (to == "html") {
         string = html(split);
@@ -132,6 +139,29 @@ function encrypt_vigenere(s: string, key: string): string {
     return result;
 }
 
+function decrypt_vigenere(s: string, key: string): string {
+    let result = "";
+    let nonNumerical = 0;
+    for (let i = 0; i < s.length; i++) {
+        let c = s.charCodeAt(i);
+        let k = key.charCodeAt((i-nonNumerical)% key.length);
+        
+        if (isAlphabetic(s.charAt(i))) {
+            let stringOffset = isUpperCase(s.charAt(i)) ? 65:97;
+            let keyOffset = isUpperCase(key.charAt((i-nonNumerical) % key.length)) ? 65 : 97;
+            
+
+            let decryptedCharCode = (c - stringOffset - (k - keyOffset) + 26) % 26 + stringOffset;
+
+            result += String.fromCharCode(decryptedCharCode);
+        } else {
+            result += s.charAt(i);
+            nonNumerical++;
+        }
+    }
+    return result;
+}
+
 function isUpperCase(char: string): bool {
     let code = char.charCodeAt(0);
     return code >= 65 && code <= 90;
@@ -145,6 +175,6 @@ function isAlphabetic(char: string): bool {
 
 
 function manifest(): void {
-    Console.log('{"name": "vigenere" ,"version":"1.0","description":"this package support vigenere modules which can be used to encrypt text","transforms":[{"from":"vigenere","to":["html","latex"],"arguments":[{"name":"key","default":"","description":"key used to decrypt text"}]}]}');
+    Console.log('{"name": "vigenere" ,"version":"1.1.0","description":"this package support vigenere modules which can be used to encrypt text","transforms":[{"from":"vigenere","to":["html","latex"],"arguments":[{"name":"key","default":"key","description":"key used to decrypt text", "type": "string"},{"name":"decrypt", "default": "false", "description": "set to true to decrypt instead of encrypt", "type": ["true", "false"]}]}]}');
     proc_exit(0);
 }

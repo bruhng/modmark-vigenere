c3905e5e195bab23a9cbd4f7c69356ad0a10dca5
bruhn <bruhng@student.chalmers.se>
2023-05-08 16:18:10 +0200

    1.0

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,24 +2,29 @@ import "../node_modules/as-wasi/assembly/index";
 import { JSON } from "../node_modules/assemblyscript-json/assembly/index";
 
 import {Console, CommandLine} from "../node_modules/as-wasi/assembly/";
+import { proc_exit } from "@assemblyscript/wasi-shim/assembly/bindings/wasi_snapshot_preview1";
+import { Obj, Value} from "assemblyscript-json/assembly/JSON";
+
 
 
 main();
 
-
 export function main(): void {
     
-    const args = new CommandLine();    
+    const args = new CommandLine().args;    
+    
+    const action = args[1];
 
-    const action = args.get(1);
+
+
     if (action == "manifest") {
         manifest();
     } else if (action == "transform") {
-        transform(args.get(2)!, args.get(3)!);
+        transform(args[2], args[3]);
     } else {
-        Console.error("invalid action " + action!)
+        Console.log("invalid action " + action)
     }
-
+    proc_exit(0);
 }
 
 function transform(from: string, to: string): void {
@@ -34,6 +39,7 @@ function transform_vigenere(to: string): void {
     const stdin: string | null = Console.readAll();
     if (stdin == null) {
         Console.error("stdin is null");
+        proc_exit(1);
         return;
     }
 
@@ -46,14 +52,62 @@ function transform_vigenere(to: string): void {
 
     let string = "";
 
+    let split = encrypted.split("\n");
+
     if (to == "html") {
-        string = '[{"name": "raw", "data": "<p>' + encrypted + '</p>"}]'
+        string = html(split);
     }else if(to == "latex") {
-        string = '[{"name": "raw", "data": ' + encrypted + '}]'
+        string = latex(split);
     }
     
     Console.log(string);
 }
+ 
+function html(lines: string[]) : string{
+    let jsonArr = new JSON.Arr();
+    for (let i = 0; i < lines.length; i++) {
+        jsonArr.push(raw("<p>"+lines[i]+"</p>"));
+    }
+    return jsonArr.stringify();
+}
+
+function latex(lines: string[]) : string{
+    let jsonArr = new JSON.Arr();
+    for (let i = 0; i < lines.length; i++) {
+        jsonArr.push(raw(escape_latex(lines[i])));
+        jsonArr.push(raw("\\\\"));
+    }
+    return jsonArr.stringify();
+}
+
+function raw(text: string): JSON.Obj {
+    let obj = new JSON.Obj();
+    obj.set("name", "raw");
+    obj.set("data", text);
+    return obj;
+}
+
+function escape_latex(text: string) : string{
+    let result : string= text.split('\\')
+    .map((t: string) =>
+      t.replace('{', '\\{')
+        .replace('}', '\\}')
+    )
+    .join('\\textbackslash{}')
+    .replace('#', '\\#')
+    .replace('$', '\\$')
+    .replace('%', '\\%')
+    .replace('&', '\\&')
+    .replace('_', '\\_')
+    .replace('<', '\\textless{}')
+    .replace('>', '\\textgreater{}')
+    .replace('~', '\\textasciitilde{}')
+    .replace('^', '\\textasciicircum{}');
+
+    return result;
+}
+
+
 
 function encrypt_vigenere(s: string, key: string): string {
     let result = "";
@@ -91,5 +145,6 @@ function isAlphabetic(char: string): bool {
 
 
 function manifest(): void {
-    Console.log('{"name":"vigenere","version":"0.1","description":"this package support vigenere modules which can be used to encrypt text","transforms":[{"from":"vigenere","to":["html","latex"],"arguments":[{"name":"key","default":"","description":"key used to decrypt text"}]}]}');
+    Console.log('{"name": "vigenere" ,"version":"1.0","description":"this package support vigenere modules which can be used to encrypt text","transforms":[{"from":"vigenere","to":["html","latex"],"arguments":[{"name":"key","default":"","description":"key used to decrypt text"}]}]}');
+    proc_exit(0);
 }
